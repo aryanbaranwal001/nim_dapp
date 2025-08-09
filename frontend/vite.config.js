02acfd34bb1360c3a5a8768f54887805b9a535ec
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-08-10 02:04:34 +0530

    frontend sorted

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173
-  }
-})
+  plugins: [tailwindcss()],
+});
